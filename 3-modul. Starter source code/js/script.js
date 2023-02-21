d27efde0e1717c3bb6a0d9d290840e077449a8fa
Akobir5 <toshtemirovakobir8@gmail.com>
2023-02-22 01:04:35 +0500

    add function for tabs

--- 3-modul. Starter source code/js/script.js
+++ 3-modul. Starter source code/js/script.js
@@ -1,5 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-  
+  const tabParent = document.querySelector('.tabheader__items');
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabContent = document.querySelectorAll('.tabcontent');
+
+  function hideTabContent() {
+    tabContent.forEach(item => {
+      item.style.display = "none";
+    });
+    tabs.forEach(item => {
+      item.style.display = "none";
+    })
+  }
+
+  hideTabContent();
+
+  function showTabContent() {
+
+  }
 
 })
