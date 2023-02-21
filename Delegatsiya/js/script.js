1a6d56192b098f613c3abb8c83f3f9a81928f039
Akobir5 <toshtemirovakobir8@gmail.com>
2023-02-22 00:40:44 +0500

    add JS Delegatsiya #matches #target #event

--- Delegatsiya/js/script.js
+++ Delegatsiya/js/script.js
@@ -3,25 +3,23 @@ const btnMain = document.getElementById("btn-main");
 btnMain.addEventListener('click', (event) => {
   // console.dir(event.target);
 
-  if(event.target && event.target.tagName == 'BUTTON') {
-    if(event.target.classList.contains('blue')) {
+  if (event.target && event.target.tagName == 'BUTTON') {
+    if (event.target.classList.contains('blue')) {
       console.log("Blue btn is clicked");
     }
-    if(event.target.classList.contains('red')) {
+    if (event.target.classList.contains('red')) {
       console.log("Red btn is clicked");
     }
-    else if(!event.target.classList.contains('blue') && !event.target.classList.contains('red')) {
+    else if (!event.target.classList.contains('blue') && !event.target.classList.contains('red')) {
       console.log("Yellow btn is clicked");
     }
   }
 })
 
-const btns = document.querySelectorAll('.btns');
+const btns = document.querySelectorAll('.btn');
 
-btns.forEach(item => {
-  item.addEventListener('click', () => {
-    console.log("Click!!!");
-  })
+btns[0].addEventListener('click', () => {
+  btns[3].classList.toggle('red');
 })
 
 const redBtn = document.createElement('button');
