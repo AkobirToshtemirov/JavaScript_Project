f3647a5b822493706ca02c3a6c41ba08dc2f55ef
Akobir5 <toshtemirovakobir8@gmail.com>
2023-02-21 16:00:00 +0500

    add all changes

--- Class JS/js/script.js
+++ Class JS/js/script.js
@@ -1,2 +1,22 @@
 "use strict";
 
+class Car {
+  constructor(name, color, speed) {
+    this.name = name;
+    this.color = color;
+    this.speed = speed;
+  }
+
+  calcSpeed() {
+    return this.speed * 100;
+  }
+
+  getColor() {
+    return this.color;
+  }
+}
+
+const mers = new Car('S500', 'red', 1000);
+console.log(mers);
+console.log(mers.calcSpeed());
+console.log(mers.getColor());
