84fa1ebcc88bbd6c14662ff2c4ad160586fde786
Akobir5 <toshtemirovakobir8@gmail.com>
2023-02-22 00:23:58 +0500

    add event target

--- Delegatsiya/js/script.js
+++ Delegatsiya/js/script.js
@@ -3,17 +3,27 @@ const btnMain = document.getElementById("btn-main");
 btnMain.addEventListener('click', (event) => {
   // console.dir(event.target);
 
-  // if(event.target && event.target.tagName == 'BUTTON') {
-  //   console.log("btn is clicked");
-  // }
-
-  if(event.target && event.target.classList.contains('blue__btn')) {
-    console.log("Blue btn is clicked");
+  if(event.target && event.target.tagName == 'BUTTON') {
+    if(event.target.classList.contains('blue')) {
+      console.log("Blue btn is clicked");
+    }
+    if(event.target.classList.contains('red')) {
+      console.log("Red btn is clicked");
+    }
+    else if(!event.target.classList.contains('blue') && !event.target.classList.contains('red')) {
+      console.log("Yellow btn is clicked");
+    }
   }
 })
 
 const btns = document.querySelectorAll('.btns');
 
 btns.forEach(item => {
-  
+  item.addEventListener('click', () => {
+    console.log("Click!!!");
+  })
 })
+
+const redBtn = document.createElement('button');
+redBtn.classList.add('red');
+btnMain.appendChild(redBtn);
