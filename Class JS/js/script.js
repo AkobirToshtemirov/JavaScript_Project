7099c7219cb14507ecbd368ac30e3e9c0c41ffad
Akobir5 <toshtemirovakobir8@gmail.com>
2023-02-21 23:15:53 +0500

    add js class

--- Class JS/js/script.js
+++ Class JS/js/script.js
@@ -19,10 +19,22 @@ class Car {
 // extends means this class is a child of another class
 
 class Spark extends Car {
+  constructor (name, color, speed, isAirbag, extraBallon) {
+    super(name, color, speed)
+    this.isAirbag = isAirbag;
+    this.extraBallon = extraBallon;
+  }
 
+  logger() {
+    console.log(`Name of the car is ${this.name}. Color is ${this.color}. Speed is ${this.speed}km/h. Airbag is ${this.isAirbag}. ExtraBallon is ${this.extraBallon}`);
+  }
 }
 
-const mers = new Car('S500', 'red', 1000);
-console.log(mers);
-console.log(mers.calcSpeed());
-console.log(mers.getColor());
+// const mers = new Car('S500', 'red', 1000);
+// console.log(mers);
+// console.log(mers.calcSpeed());
+// console.log(mers.getColor());
+
+const spark = new Spark("Spark", "Red", 200, "Available", "Availbale 4");
+spark.logger();
+console.log(spark);
