9b7a045b7ea29aca34e87a1354cf341d3db57843
Akobir5 <toshtemirovakobir8@gmail.com>
2023-02-24 00:48:56 +0500

    getComputedStyle

--- Window & Document JS/js/script.js
+++ Window & Document JS/js/script.js
@@ -1,2 +1,24 @@
 const box = document.querySelector('.box');
-const btn = document.querySelector('.btn')
+const btn = document.querySelector('.btn');
+
+// clientWidth
+// clientHeight
+// offsetWidth rarely used
+// offsetHeight rarely used
+// scrollHeight
+
+const width = box.clientWidth;
+const height = box.clientHeight;
+console.log("width: " + width + "\nheight: " + height);
+
+const scHeight = box.scrollHeight;
+console.log(scHeight);
+
+btn.addEventListener('click', () => {
+  box.style.height = scHeight + 'px';
+})
+
+// Computed Style
+
+const style = getComputedStyle(box);
+console.log(style);
