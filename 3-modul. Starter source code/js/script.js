ae9fa063afed7ef2e5734e832af3163452e64cbd
Akobir5 <toshtemirovakobir8@gmail.com>
2023-02-23 17:57:01 +0500

    add loader

--- 3-modul. Starter source code/js/script.js
+++ 3-modul. Starter source code/js/script.js
@@ -3,13 +3,25 @@ window.addEventListener("DOMContentLoaded", () => {
   const tabsParent = document.querySelector('.tabheader__items');
   const tabs = document.querySelectorAll('.tabheader__item');
   const tabsContent = document.querySelectorAll('.tabcontent');
+  const loader = document.querySelector('.loader');
+
+  // Loader
+
+  setTimeout(() => {
+    loader.style.opacity = '0';
+    setTimeout(() => {
+      loader.style.display = 'none';
+    }, 500)
+  }, 2000)
+
+  // Tabs
 
   function hideTabContent() {
     tabsContent.forEach(item => {
       item.classList.add('hide');
       item.classList.remove('show', 'fade');
     });
- 
+
     tabs.forEach(item => {
       item.classList.remove('tabheader__item_active');
     })
@@ -26,11 +38,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
   tabsParent.addEventListener('click', (event) => {
     const target = event.target;
-    if(target && target.classList.contains('tabheader__item')) {
+    if (target && target.classList.contains('tabheader__item')) {
       tabs.forEach((item, idx) => {
-        if(target == item) {
+        if (target == item) {
           hideTabContent();
-          showTabContent(idx);        
+          showTabContent(idx);
         }
       })
     }
